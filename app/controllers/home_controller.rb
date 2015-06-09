bc5dd0ad7b7925e12d1839e8ba62cc701d961257
jdoper <atomas395@gmail.com>
2015-06-09 20:11:16 -0300

    Alterações no layout da aplicação

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -23,11 +23,41 @@ class HomeController < ApplicationController
   end
 
   def relatorio
-  	@registros = Registry.all
-  	@questoes = Question.all.order(:numero)
+  	registros = Registry.all
+  	questoes = Question.all.order(:numero)
 
-    respond_to do |format|
-      format.xls
+    # Montagem da estrututa da planilha
+    csv_string = CSV.generate do |csv|
+      # Cabeçalho
+      csv << ["N", "Data", "Nome", "RH", "ID", "Sexo", "Tipo de IC", "Etiologia", "CF", "FEVE", "FEVE(RI)", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "Score"]
+    
+      # Construindo linha de registro
+      registros.each do |cadastro|
+        line = []
+        line << cadastro.id
+        line << cadastro.data
+        line << cadastro.nome
+        line << cadastro.registro
+        line << cadastro.idade
+        line << cadastro.sexo
+        line << cadastro.tipoInsuficiencia
+        line << cadastro.etiologia
+        line << cadastro.classFuncional
+        line << cadastro.feve
+        line << "99"
+        questoes.each do |questao|
+          if questao.serial == cadastro.serial
+            line << questao.resposta
+          end
+        end
+        line << cadastro.score
+
+        # Escrevendo linha no arquivo
+        csv << line
+      end
     end
+
+    # Mostrando arquivo para usuário
+    send_data csv_string, :type => 'text/csv; header=present', :disposition => "attachment; filename=AMIIC.csv" 
   end
 end
